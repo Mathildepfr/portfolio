9fbed1dd149614ea96ef9315da08222d19a88cba
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-09-21 13:41:24 +1000

    projects and homepage

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def contact
+  end
+
+  def home
+  end
+
+  def artco
+  end
+
+  def hatsrats
+  end
+
+  def movies
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'artco', to: 'pages#artco', as: :artco
+  get 'hatsrats', to: 'pages#hatsrats', as: :hatsrats
+  get 'movies', to: 'pages#movies', as: :movies
 end
